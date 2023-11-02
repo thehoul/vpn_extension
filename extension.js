14e7da171225f315f4392cd81ea307789ac8998d
HOT <theo@houle.pro>
2023-11-02 20:11:25 +0100

    Check status every 5 seconds

--- extension.js
+++ extension.js
@@ -23,7 +23,7 @@ const GETTEXT_DOMAIN = 'my-indicator-extension';
 const { GObject, St } = imports.gi;
 const Gio = imports.gi.Gio;
 const Me = imports.misc.extensionUtils.getCurrentExtension();
-
+const Mainloop = imports.mainloop;
 const GLib = imports.gi.GLib;
 
 const ExtensionUtils = imports.misc.extensionUtils;
@@ -47,6 +47,7 @@ const disconnected_icon = new St.Icon({
 
 const base_cmd = 'nmcli connection'
 
+let timeout;
 
 const Indicator = GObject.registerClass(
 class Indicator extends PanelMenu.Button {
@@ -65,7 +66,7 @@ class Indicator extends PanelMenu.Button {
 
         let item = new PopupMenu.PopupMenuItem(_('Connect to vipiN'));
         item.connect('activate', () => {
-            this.toggle_connect(target);
+            this._toggle_connect(target);
         });
 
         this.menu.addMenuItem(item);
@@ -75,20 +76,17 @@ class Indicator extends PanelMenu.Button {
      * Set the connection of the given target to up if down or to down if up
      * @param {string} target name of the target connection to toggle
      */
-    toggle_connect(target){
+    _toggle_connect(target){
         let cmd = base_cmd + " " + (this.connected?'down':'up') + " " + this.target;
         console.log(cmd);
         var [ok, out, err, _] = GLib.spawn_command_line_sync(cmd);
-        console.log(ok, out.toString(), err.toString());
-        console.log("Connected:", this.connected);
 
         // Check if the cmd worked
         if(out.length == 0 && err.length > 0){
             // Didn't work so report the error we got and do nothing else
-            this.report_error(err.toString());
+            Indicator.report_error("Couldn't toggle VPN connection", err.toString());
         } else {
-            this.connected = !this.connected;
-            this.update_icon();
+            this._update_state(!this.connected);
         }
     }
 
@@ -96,15 +94,34 @@ class Indicator extends PanelMenu.Button {
      * Show an error notification containing the given error text.
      * @param {string} err_str Error string representation
      */    
-    report_error(err_str){
-        let title = this.connected?"Couldn't disconnect from the VPN":"Couldn't connect to the VPN";
+    static report_error(title, err_str){
         Main.notifyError(title, err_str);
+        console.log(title + ": ", err_str);
+    }
+
+    static report_info(title, message){
+        Main.notify(title, message);
+        console.log(title+": ", message);
+    }
+
+    /**
+     * Update the connection state to be the one given. This takes care of updating everything needed in the object (for instace the icon)
+     * @param {boolean} new_state new state connetion state (true = connected, false = disconnected)
+     * @returns {boolean} true if the state was update and false if the given state is already the current one
+     */
+    _update_state(new_state){
+        if(this.connected == new_state){
+            return false;
+        }
+        this.connected = new_state;
+        this._update_icon();
+        return true;
     }
 
     /**
      * Modify icon to match the current state of the connection
      */
-    update_icon(){
+    _update_icon(){
         this.remove_all_children();
         if(this.connected){
             this.add_child(disconnected_icon);
@@ -112,8 +129,32 @@ class Indicator extends PanelMenu.Button {
             this.add_child(connected_icon);
         }
     }
-});
 
+    _parse_state(state_str){
+        lines = state_str.split("\n");
+        lines.forEach((line) => {
+            tokens = line.split("\t");
+            if(tokens[0] == this.target){
+                return true;
+            }
+        });
+        // If the loop didn't return then the connection wasn't active and thus return false
+        return false;
+    }
+
+    check_state(){
+        var [ok, out, err, _] = GLib.spawn_command_line_sync('nmcli connection show --active');
+        if(ok && err.length == 0){
+            let state = this._parse_state(out.toString());
+            if(this._update_state(state)){
+                Indicator.report_info("Connection state updated", "The state of the connection ${target} changed unexpectedly to ${state}");
+            }
+        } else {
+            Indicator.report_error("Couldn't verify connection state", err.toString());
+        }
+        return true;
+    }
+});
 
 
 class Extension {
@@ -127,11 +168,13 @@ class Extension {
     enable() {
         this._indicator = new Indicator('vipiN');
         Main.panel.addToStatusArea(this._uuid, this._indicator);
+        timeout = Mainloop.timeout_add_seconds(5.0, this._indicator.check_state);
     }
 
     disable() {
         this._indicator.destroy();
         this._indicator = null;
+        Mainloop.source_remove(timeout);
     }
 }
 
